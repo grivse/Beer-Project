a63469305dd99f0158a8a85ecf00fc4075c622fa
grivse <grivse@gmail.com>
2019-02-14 15:17:04 +0200

    add beer-project

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css'
+
+const Main = () => {
+    return (
+        <div className="wrapper">
+            <div className='container'>
+
+            </div>
+        </div>
+
+    );
+};
+
+export default Main;
